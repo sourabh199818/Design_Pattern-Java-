8ce1dad3d2e4b0bb2e5090d007486a66ecb506c3
Sourabh Bisht <68646654+sourabh199818@users.noreply.github.com>
2023-08-08 14:57:23 +0530

    Creation of class

---
+++ Factory_Pattern/Main.java
@@ -0,0 +1,10 @@
+package Factory_Pattern;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        System.out.println("Factory Method");
+
+    }
+}
---
+++ Singleton_Pattern/Main.java
@@ -0,0 +1,10 @@
+package Singleton_Pattern;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        System.out.println("Singleton Method");
+
+    }
+}
